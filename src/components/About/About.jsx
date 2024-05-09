333b1ff7e3a6466e0219d4c87a55a18f9e49dca7
aadhi-11 <adithyadasaadhiii@gmail.com>
2024-05-09 10:47:17 +0530

    about on progress

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Style from './About.module.css'
+import { Button } from '@mui/material'
+import macbook from '../../Assets/Images/profile-removebg-preview (1).png'
 
 const About = () => {
   return (
     <div className={Style.Main} >
-        This is about
+      <div className={Style.left} >
+        <div className={Style.frame} >
+          <img src={macbook} alt="Home Page Image" className={Style.img} />
+        </div>
+      </div>
+      <div className={Style.Right} >
+        <h3 className={Style.im} >I'M</h3>
+        <h1 className={Style.Name} >ADITHYADAS U S</h1>
+        <h2 className={Style.Description} >FULLSTACK WEB DEVELOPER</h2>
+        <div className={Style.BtnReadMore} >Read More ...</div>
+      </div>
     </div>
   )
 }
---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          grid: "grid 15s linear infinite",
+        },
+        keyframes: {
+          grid: {
+            "0%": { transform: "translateY(-50%)" },
+            "100%": { transform: "translateY(0)" },
+          },
+        },
+      },
+    },
+  };
+  
