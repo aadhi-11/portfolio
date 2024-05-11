fb665cedfc17b9cfdb29a44d1d990792b3acb5a1
aadhi-11 <adithyadasaadhiii@gmail.com>
2024-05-11 11:59:19 +0530

    education started

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import styles from './Education.module.css'
-
+import PublicIcon from '@mui/icons-material/Public';
+import educover from '../../Assets/Images/HT Code-a-thon_ Website Design - HT School.jpeg'
 const Education = () => {
   return (
     <div className={styles.eduMain} >
-        This is education
+      <div className={styles.eduContainer} >
+        <div className={styles.educard} >
+          <div className={styles.up} >
+            <div className={styles.cover} >BCA</div>
+          </div>
+          <div className={styles.down} >
+            <h1 className={styles.course} >Bachelor Of computer Applications</h1>
+            <p><i>from</i></p>
+            <h3>Yeldo Mar Baselios college ,Kothamangalam<span><PublicIcon /></span> </h3>
+            <p>Affiliated to Mahatma gandhi University,Kottayam</p>
+          </div>
+        </div>
+
+        <div className={styles.educard} >
+          <div className={styles.up} >
+            <div className={styles.cover} >+2</div>
+          </div>
+          <div className={styles.down} >
+            <h1 className={styles.course} >Higher Secondary Education(+2)</h1>
+            <p><i>from</i></p>
+            <h3>HSS Vallappuzha,Palakkad<span><PublicIcon /></span> </h3>
+            <p>Higher Secondary In science Stream</p>
+          </div>
+        </div>
+
+        <div className={styles.educard} >
+          <div className={styles.up} >
+            <div className={styles.cover} >10</div>
+          </div>
+          <div className={styles.down} >
+            <h1 className={styles.course} >High School(10th)</h1>
+            <p><i>from</i></p>
+            <h3>HSS Vallappuzha,Palakkad<span><PublicIcon /></span> </h3>
+            <p>Higher Secondary In science Stream</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
