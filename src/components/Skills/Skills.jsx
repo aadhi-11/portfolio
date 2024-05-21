a353ba03261aaf6835f646e5a517a238c99ff9b0
aadhi-11 <adithyadasaadhiii@gmail.com>
2024-05-21 18:21:49 +0530

    portfolio on progress

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,18 +7,127 @@ const Skills = () => {
         <div className={Styles.container} >
             <div className="title">Skills</div>
             <div className={Styles.inner} >
-                <div className={Styles.each} >
-                    <CircularProgressbar value={50} maxValue={100} text={`40%`} className={Styles.progress}  />
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Javascript
+                    </div>
                 </div>
-                <div className={Styles.each} >
-                    <CircularProgressbar value={50} maxValue={100} text={`40%`} className={Styles.progress}  />
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Node Js
+                    </div>
                 </div>
-                <div className={Styles.each} >
-                    <CircularProgressbar value={50} maxValue={100} text={`40%`} className={Styles.progress}  />
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        React Js
+                    </div>
                 </div>
-                <div className={Styles.each} >
-                    <CircularProgressbar value={50} maxValue={100} text={`40%`} className={Styles.progress}  />
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Express Js
+                    </div>
                 </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Python
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Django
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Flask
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Pandas
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Java
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Php
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        C
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        C++
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        HTML
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        CSS
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Module css
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        SCSS
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Tailwind css
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        TensorFlow
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        MatplotLib
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Git
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Github
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Google Cloud
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        Digital ocean
+                    </div>
+                </div>
+                <div className={Styles.row} >
+                    <div className={Styles.each} >
+                        AWS
+                    </div>
+                </div>
+
             </div>
         </div>
     )
